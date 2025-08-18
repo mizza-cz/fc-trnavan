56bd1aa875bc125acbc7d993a7d2f9f2980d0cba
mizza-cz <mizza.cz666@gmail.com>
2025-08-18 08:02:42 +0200

    fix js

--- dist/js/load-articles.js
+++ dist/js/load-articles.js
@@ -1,26 +1,35 @@
+// $(function () {
+//     $('[data-a-category]').on('show.bs.tab', function (e) {
+//         var category = $(this).data('a-category');
+//         if (!$(this).hasClass('a-loaded')) {
+//             $.ajax({
+//                 url: '/inc/article-load.php?category='+category,
+//                 dataType: 'json',
+//                 success: function (_response) {
+//                     var error = _response.error;
+//                     var output_data = _response.output_data;
+//                     if (error != "") {
+//                         alert(error);
+//                     }
+//                     else {
+//                         $('#'+category+'-pane').append(output_data);
+//                         $('#'+category+'-tab').addClass('a-loaded');
+//                     }
+//                 },
+//                 error: function (jqXhr, json, errorThrown) {
+//                     var error = jqXhr.responseText;
+//                     alert(error);
+//                 }
+//             });
+//         }
+//     })
+// });
+
 $(function () {
-    $('[data-a-category]').on('show.bs.tab', function (e) {
-        var category = $(this).data('a-category');
-        if (!$(this).hasClass('a-loaded')) {
-            $.ajax({
-                url: '/inc/article-load.php?category='+category,
-                dataType: 'json',
-                success: function (_response) {
-                    var error = _response.error;
-                    var output_data = _response.output_data;
-                    if (error != "") {
-                        alert(error);
-                    }
-                    else {
-                        $('#'+category+'-pane').append(output_data);
-                        $('#'+category+'-tab').addClass('a-loaded');
-                    }
-                },
-                error: function (jqXhr, json, errorThrown) {
-                    var error = jqXhr.responseText;
-                    alert(error);
-                }
-            });
-        }
-    })
+  $("[data-a-category]").on("click", function (e) {
+    e.preventDefault();
+
+    var tabTrigger = new bootstrap.Tab(this);
+    tabTrigger.show();
+  });
 });
